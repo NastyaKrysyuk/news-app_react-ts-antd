2e343709a425ec72dd68a6bc25eabfd3fb5c7ba9
Nastya <nkrysyuk@inbox.ru>
2022-07-06 22:33:22 +0300

    fix: some fix

--- src/components/news-item/news-item.tsx
+++ src/components/news-item/news-item.tsx
@@ -1,5 +1,6 @@
 
-import { Button, Card, Col, Row } from 'antd';
+import { DeleteOutlined } from '@ant-design/icons';
+import { Button, Card, Col, Popover, Row } from 'antd';
 import './style.css'
 
 const NewsItem = ({ title, description, url, urlToImage }: {
@@ -9,18 +10,26 @@ const NewsItem = ({ title, description, url, urlToImage }: {
   urlToImage: string,
 }) => {
   return (
-    <a href={url} className="site-card-wrapper" style={{ backgroundImage: `url(${urlToImage})`}}>
-      <Row gutter={1} style={{display:'block'}}>
-        <Col >
+    <div className="site-card-wrapper" style={{ backgroundImage: `url(${urlToImage})` }}>
+      <a href={url}>
+        <Row gutter={1} style={{ display: 'block' }}>
+          <Col >
             <Card className='card-news' title={title} bordered={false} >
               {description}
             </Card>
-        </Col>
-      </Row>
-      <Button className='btn-more' type="primary" shape="round" >
-            view more
-      </Button>
-    </a>
+          </Col>
+        </Row>
+        <Button className='btn-more' type="primary" shape="round" onClick={(_e: any) => { _e.preventDefault() }}>
+          add to reading list
+        </Button>
+      </a>
+      <Popover content='Hide'>
+        <Button type="primary" shape="round" className='btn-del' onClick={(_e: any) => { _e.preventDefault() }}>
+          <DeleteOutlined />
+        </Button>
+      </Popover>
+    </div>
+
   )
 }
 
