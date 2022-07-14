ad702f03a74a0c592fffc701e16d6938973ee358
NastyaKrysyuk <nkrysyuk@inbox.ru>
2022-07-14 20:53:44 +0300

    fix: auth

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,16 @@
 import React from 'react';
 import './App.css';
 import Header from './components/header';
-import { BrowserRouter} from "react-router-dom";
 import './firebase';
 import AppRouting from './routing/routing';
 
-const App: React.FC = () => (
-  <BrowserRouter>
-    <Header />
-    <AppRouting/>
-  </BrowserRouter>
-);
+const App: React.FC = () => {
+  return (
+    <>
+      <Header />
+      <AppRouting/>
+    </>
+  )
+};
 
 export default App;
--- src/routing/routing.tsx
+++ src/routing/routing.tsx
@@ -1,13 +1,15 @@
 import { TRoute } from "../type/type";
 import { Paths } from "./path";
 
-import { useRoutes } from "react-router-dom";
+import { useLocation, useNavigate, useRoutes } from "react-router-dom";
 import HomePage from "../pages/Home";
 import LoginPage from "../pages/Login";
 import RegisterPage from "../pages/Register";
 import ReadingListPage from "../pages/ReadingList";
 import AddNewsPage from "../pages/AddNews";
 import Article from "../pages/Article";
+import { useAuth } from "../hook/use-auth";
+import { useEffect } from "react";
 
 export const HomeRoutes: TRoute[] = [
   { path: Paths.main, element: <HomePage />, name: 'Home' },
@@ -22,8 +24,18 @@ export const Routes: TRoute[] = [
   { path: Paths.addnews, element: <AddNewsPage />, userRole: 'admin', name: 'AddNews' },
 ]
 
-function AppRouting() {
-
+function RoutesCore() {
   return useRoutes(Routes)
+}
+function AppRouting() {
+  const navigate = useNavigate()
+  const { isAuth } = useAuth();
+  let location = useLocation();
+  useEffect(() => {
+    if (!isAuth) {
+      navigate('/login', {state: {from: location}})
+    }
+  }, [isAuth])
+  return <RoutesCore/>
 }
 export default AppRouting
