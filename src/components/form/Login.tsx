26a9bf10f785b6bfb2c5749eea2f0fd2dd32b061
NastyaKrysyuk <nkrysyuk@inbox.ru>
2022-07-14 10:27:59 +0300

    add: check is auth

--- src/components/form/Login.tsx
+++ src/components/form/Login.tsx
@@ -4,14 +4,28 @@ import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
 import FormComponent from "./Form";
 import {setUser} from '../../store/slices/authSlices'
 import { useAppDispatch } from "../../hook/redux-hooks";
+import {useEffect} from "react";
 
 
 const Login=()=>{
   const dispatch = useAppDispatch();
-  const push = useNavigate();
-  
+  const navigate = useNavigate();
+    const auth = getAuth();
+    useEffect(() => {
+        auth.onAuthStateChanged(user => {
+            if (user?.email) {
+                dispatch(setUser({
+                    email: user.email,
+                    id: user.uid,
+                    token: user.refreshToken,
+                }));
+                navigate('/', { replace: true });
+            }
+        })
+
+    }, []);
+
   const handleLogin = (email: string, password: string) => {
-      const auth = getAuth();
       signInWithEmailAndPassword(auth, email, password)
           .then(({user}) => {
               console.log(user);
@@ -20,7 +34,7 @@ const Login=()=>{
                   id: user.uid,
                   token: user.refreshToken,
               }));
-              push('/');
+              navigate('/', { replace: true });
           })
           .catch(() => alert('Invalid user!'))
   }
