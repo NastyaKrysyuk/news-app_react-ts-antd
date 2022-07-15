ea71c8a6432cae03a145fb42750963202fe26359
Nastya <nkrysyuk@inbox.ru>
2022-07-15 10:24:59 +0300

    add: form for add news

--- src/pages/AddNews/addNewsPage.tsx
+++ src/pages/AddNews/addNewsPage.tsx
@@ -1,4 +1,69 @@
+import { Button, DatePicker, Form, Input, InputNumber, Typography } from 'antd';
+import './style.css'
+
+const { Title } = Typography;
+
+const layout = {
+  labelCol: { span: 10 },
+  wrapperCol: { span: 16 },
+};
+
+/* eslint-disable no-template-curly-in-string */
+const validateMessages = {
+  required: '${label} is required!',
+  types: {
+    email: '${label} is not a valid email!',
+    number: '${label} is not a valid number!',
+  },
+  number: {
+    range: '${label} must be between ${min} and ${max}',
+  },
+};
+/* eslint-enable no-template-curly-in-string */
 const AddNewsPage=()=>{
-return null
+  const onFinish = (values: any) => {
+    console.log(values);
+  };
+return (
+  <div className='wrapper-news-list'>
+  <Title>Add news:</Title>
+    <Form {...layout} name="nest-messages" onFinish={onFinish} validateMessages={validateMessages}>
+      <Form.Item name={ 'title'} label="Title" rules={[{ required: true }]}>
+        <Input />
+      </Form.Item>
+     
+      <Form.Item name={ 'description'} label="Description" rules={[{ required: true }]}>
+        <Input />
+      </Form.Item>
+
+      <Form.Item name={'author'} label="Author" rules={[{ required: true }]}>
+        <Input />
+      </Form.Item>
+
+      <Form.Item name={'publishAt'} label="Date" rules={[{ required: true }]}>
+        <DatePicker />
+      </Form.Item>
+      
+      <Form.Item name={ 'url'} label="Link article" rules={[{ required: true }]}>
+        <Input />
+      </Form.Item>
+
+      <Form.Item name={'urlToImage'} label="Link image" rules={[{ required: true }]}>
+        <Input />
+      </Form.Item>
+
+      <Form.Item name={ 'content'} label="Content">
+        <Input.TextArea />
+      </Form.Item>
+
+
+      <Form.Item wrapperCol={{ ...layout.wrapperCol, offset: 10 }}>
+        <Button type="primary" htmlType="submit" >
+        Post news
+        </Button>
+      </Form.Item>
+    </Form>
+  </div>
+)
 }
 export default AddNewsPage
--- src/pages/ReadingList/readingListPage.tsx
+++ src/pages/ReadingList/readingListPage.tsx
@@ -26,8 +26,6 @@ const ReadingListPage = () => {
       } />}
       {arr && <NewsItem articles={arr} />}
     </div>
-
-
   )
 }
 
