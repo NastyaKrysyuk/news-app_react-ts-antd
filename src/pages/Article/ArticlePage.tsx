21ecf5127724709b59adffbb759e3a7707a131e4
Nastya <nkrysyuk@inbox.ru>
2022-07-15 10:24:59 +0300

    add: form for add news

--- src/pages/Article/ArticlePage.tsx
+++ src/pages/Article/ArticlePage.tsx
@@ -3,15 +3,18 @@ import { Button, Card, Image, Typography } from 'antd';
 import './style.css'
 import { LinkOutlined } from "@ant-design/icons";
 import { url } from "inspector";
-const { Text } = Typography;
+const { Text, Title } = Typography;
 const ArticlePage = () => {
   const article = useAppSelector((state) => state.newsList.article)
   return (
     <article className="article">
-      <Card title={article?.title} bordered={false} style={{ width: '60%', margin: '5rem auto' , borderRadius:'15px'}}>
-        <p className="description">{article?.description}</p>
+      <Card title={article?.title} bordered={false} >
+        <Title >{article?.description}</Title>
+        <div className="pablish-date">
         <Text type="secondary" italic={true} >by {article?.author}</Text><br/>
-        <Text type="secondary" italic={true} >{article?.publishedAt.toString().slice(0,10)}</Text>
+        <Text  type="secondary" italic={true} >{article?.publishedAt.toString().slice(0,10)}</Text>
+        </div>
+
         <Image
           src={article?.urlToImage}
         />
